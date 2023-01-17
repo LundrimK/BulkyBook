3c588cfd07f3e1408968d0df05effa508fcc080b
LundriK <lundrimkrasniqi02@gmail.com>
2023-01-17 23:07:54 +0100

    Ndryshimet1/17/23

---
+++ BulkyBulWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,67 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using DocumentFormat.OpenXml.Office2013.Drawing.ChartStyle;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+            orderHeaders = unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+
+            if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                orderHeaders = unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim=claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeaders = unitOfWork.OrderHeader.GetAll(u=>u.ApplicationUserId==claim.Value,includeProperties: "ApplicationUser");
+
+            }
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u=>u.PaymentStatus==SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                     orderHeaders=orderHeaders.Where(u=>u.OrderStatus==SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u=>u.OrderStatus==SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                case "all":
+                    break;
+            }
+            
+            return Json(new { data = orderHeaders });
+        }
+        #endregion
+    }
+}
